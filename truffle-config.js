639c034fbfffe804b14def99feb79609b33f4f8f
mahanbehroo <mahan.behroo@gmail.com>
2021-04-16 04:28:11 +0430

    kovan network added

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 
-
 const HDWalletProvider = require('truffle-hdwallet-provider');
 
 
@@ -13,9 +12,18 @@ networks: {
         "https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}"
       )
     },
-    gasPrice: 25000000000,
-    network_id: "3"
+    gasPrice: 25000000000000,
+    network_id: "3",
+    networkCheckTimeout: 50000
   },
+
+  kovan: {
+	    provider: new HDWalletProvider(process.env.DEPLOYMENT_ACCOUNT_KEY, "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY),
+	    network_id: 42,
+	    gas: 5000000,
+			gasPrice: 5000000000, // 5 Gwei
+			skipDryRun: true
+		},
 
 development: {
   host: "http://127.0.0.1",
